14f30e1b2f304046c461adc7f935faa8328dea33
riansyah <mriansyah93@gmail.com>
2018-02-25 14:12:44 +0700

    cancan repair

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :destroy, Product do |product|
-      product.merchant.user == user
+    if user
+      can :destroy, Product do |product|
+        product.merchant.user == user
+      end
     end
   end
 end
