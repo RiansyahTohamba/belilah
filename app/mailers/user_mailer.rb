3841da07f0ec02c5bfe1c5f249858d9b593d119a
riansyahTohamba <mriansyah93@gmail.com>
2018-03-01 09:53:52 +0700

    delaying to deliver attachment feature

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ApplicationMailer
 
   def purchase_mail(order,email_to)
     @order = order
-    attachments["order_#{@order.id}.pdf"] = WickedPdf.new.pdf_from_string(
-      render_to_string(pdf: 'receipt',
-            template: 'purchase_mail.html.erb',
-            layout: 'pdf.html'), { :hash_with_wickedpdf_options }
-    )
+    # attachments["order_#{@order.id}.pdf"] = WickedPdf.new.pdf_from_string(
+    #   render_to_string(pdf: 'receipt',
+    #         template: 'purchase_mail.html.erb',
+    #         layout: 'pdf.html'), { :hash_with_wickedpdf_options }
+    # )
     mail(to: email_to, subject: "[Belilah] Pesanan Anda")
   end
 end
