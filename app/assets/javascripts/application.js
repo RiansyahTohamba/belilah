97e56ce28cdfc56751c7a1e900ada175de53fc04
riansyahTohamba <mriansyah93@gmail.com>
2018-03-02 10:23:11 +0700

    remove js require_tree

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,5 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require popper
-//= require bootstrap
-//= require jquery
-//= require jquery-ui
 //= require turbolinks
-//= require_tree .
+//= require jquery
