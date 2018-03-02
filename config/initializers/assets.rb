b8e6de9e7898e484e44191e55a9dc216bbe84dc7
riansyahTohamba <mriansyah93@gmail.com>
2018-03-02 10:15:07 +0700

    remove javascript file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,8 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-
-%w( cart carts charges customer home line_items merchants orders products registration   ).each do |controller|
-  # Rails.application.config.assets.precompile += ["#{controller}.js", "#{controller}.css"]
-  Rails.application.config.assets.precompile += ["#{controller}.css"]
-end
+Rails.application.config.assets.precompile += %w( active_admin.js active_admin.css cart.css carts.css charges.css customer.css home.css line_items.css merchants.css orders.css products.css registration.css )
